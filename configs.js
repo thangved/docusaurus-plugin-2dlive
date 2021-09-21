cac007e5314d074fe9964ba87beb72bf1e6595af
thangved <thangcreation@gmail.com>
2021-09-21 10:41:55 +0700

    First commit

---
+++ configs.js
@@ -0,0 +1,33 @@
+import live2dJSString from './bin/live2d'
+import React from "react";
+import ReactDOM from "react-dom";
+import Models from "./bin/Model";
+import data from './bin/data'
+
+if (!window.loadlive2d) {
+    const script = document.createElement('script')
+    script.innerText = live2dJSString
+    document.body.appendChild(script)
+}
+
+const models = document.createElement('div')
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Models data={data} />
+    </React.StrictMode>,
+    models
+)
+
+document.body.append(models)
+
+if (document.body.offsetWidth < 740) {
+    models.style.display = 'none'
+}
+
+document.body.onresize = () => {
+    if (document.body.offsetWidth < 740) {
+        return models.style.display = 'none'
+    }
+    models.style.display = 'block'
+}
