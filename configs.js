7ba88d2b19078712bf07a274cc6e597745469cc2
thangved <thangcreation@gmail.com>
2021-09-21 14:51:15 +0700

    fix

--- configs.js
+++ configs.js
@@ -2,30 +2,38 @@ import live2dJSString from './bin/live2d'
 import React from "react"
 import ReactDOM from "react-dom"
 import Models from "./bin/Model"
-import data from './bin/data'
 
-const script = document.createElement('script')
-script.innerText = live2dJSString
-document.body.appendChild(script)
+try {
+    const script = document.createElement('script')
+    script.innerText = live2dJSString
+    script.defer = true
+    document.head.appendChild(script)
 
-const models = document.createElement('div')
 
-ReactDOM.render(
-    <React.StrictMode>
-        <Models data={data} />
-    </React.StrictMode>,
-    models
-)
-
-document.body.append(models)
-
-if (document.body.offsetWidth < 740) {
-    models.style.display = 'none'
-}
-
-document.body.onresize = () => {
-    if (document.body.offsetWidth < 740) {
-        return models.style.display = 'none'
+    const models = document.createElement('div')
+    models.style = {
+        with: 0,
+        height: 0,
     }
-    models.style.display = 'block'
+
+    ReactDOM.render(
+        <React.StrictMode>
+            <Models />
+        </React.StrictMode>,
+        models
+    )
+
+    document.body.append(models)
+
+    if (document.body.offsetWidth < 740) {
+        models.style.display = 'none'
+    }
+
+    document.body.onresize = () => {
+        if (document.body.offsetWidth < 740) {
+            return models.style.display = 'none'
+        }
+        models.style.display = 'block'
+    }
 }
+catch { }
